bd509ececff09aea351a4d81d9cb716e51b5e4e0
Gerohaan <gero.delfin@gmail.com>
2022-01-02 15:54:55 -0400

    mod color

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -118,12 +118,6 @@ export default {
     return {
       loading: true,
       dialog: true,
-      colors: [
-        'linear-gradient( 135deg, #ABDCFF 10%, #0396FF 100%)',
-        'linear-gradient( 135deg, #2AFADF 10%, #4C83FF 100%)',
-        'linear-gradient( 135deg, #FFD3A5 10%, #FD6585 100%)',
-        'linear-gradient( 135deg, #EE9AE5 10%, #5961F9 100%)'
-      ]
     }
   },
   created () {
