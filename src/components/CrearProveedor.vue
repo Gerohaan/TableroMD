704b46bdd6eb5d90056f81d0fefba850716cf3eb
Gerohaan <gero.delfin@gmail.com>
2022-01-15 14:21:05 -0400

    gerohaan

--- src/components/CrearProveedor.vue
+++ src/components/CrearProveedor.vue
@@ -1,82 +1,82 @@
 <template>
     <q-card square flat>
           <q-card-section align="center">
-            <div class="text-h5 text-bold text-purple-10">Crear proveedor</div>
+            <div class="text-h5 text-bold text-primary">Crear proveedor</div>
           </q-card-section>
           <q-card-section class="q-pa-md">
-            <div class="text-left text-purple-10 text-weight-medium">
+            <div class="text-left text-primary text-weight-medium">
               Tipo de identificación
             </div>
                 <q-select :options="tipoIdentificacion" filled >
                   <template v-slot:prepend>
-                    <q-avatar color="purple-10" size="sm" font-size="17px" text-color="white" icon="badge" />
+                    <q-avatar color="primary" size="sm" font-size="17px" text-color="white" icon="badge" />
                   </template>
                 </q-select>
           </q-card-section>
           <q-card-section class="q-pa-md">
-            <div class="text-left text-purple-10 text-weight-medium">
+            <div class="text-left text-primary text-weight-medium">
               Pais
             </div>
                 <q-select filled >
                   <template v-slot:prepend>
-                    <q-avatar color="purple-10" size="sm" font-size="17px" text-color="white" icon="local_convenience_store" />
+                    <q-avatar color="primary" size="sm" font-size="17px" text-color="white" icon="local_convenience_store" />
                   </template>
                 </q-select>
           </q-card-section>
           <q-card-section class="q-pa-md">
-            <div class="text-left text-purple-10 text-weight-medium">
+            <div class="text-left text-primary text-weight-medium">
               Departamento
             </div>
                 <q-select filled >
                   <template v-slot:prepend>
-                    <q-avatar color="purple-10" size="sm" font-size="17px" text-color="white" icon="map" />
+                    <q-avatar color="primary" size="sm" font-size="17px" text-color="white" icon="map" />
                   </template>
                 </q-select>
           </q-card-section>
           <q-card-section class="q-pa-md">
-            <div class="text-left text-purple-10 text-weight-medium">
+            <div class="text-left text-primary text-weight-medium">
               Ciudad
             </div>
                 <q-select filled >
                   <template v-slot:prepend>
-                    <q-avatar color="purple-10" size="sm" font-size="17px" text-color="white" icon="location_city" />
+                    <q-avatar color="primary" size="sm" font-size="17px" text-color="white" icon="location_city" />
                   </template>
                 </q-select>
           </q-card-section>
           <q-card-section class="q-pa-md">
-            <div class="text-left text-purple-10 text-weight-medium">
+            <div class="text-left text-primary text-weight-medium">
               Dirección
             </div>
                 <q-input filled >
                   <template v-slot:prepend>
-                    <q-avatar color="purple-10" size="sm" font-size="17px" text-color="white" icon="map" />
+                    <q-avatar color="primary" size="sm" font-size="17px" text-color="white" icon="map" />
                   </template>
                 </q-input>
           </q-card-section>
-          <q-separator color="purple-10" class="q-ma-md" />
+          <q-separator color="primary" class="q-ma-md" />
           <q-card-section class="q-pa-md">
-            <div class="text-left text-purple-10 text-weight-medium">
+            <div class="text-left text-primary text-weight-medium">
               Regimen de IVA*
             </div>
                 <q-select filled >
                   <template v-slot:prepend>
-                    <q-avatar color="purple-10" size="sm" font-size="17px" text-color="white" icon="receipt_long" />
+                    <q-avatar color="primary" size="sm" font-size="17px" text-color="white" icon="receipt_long" />
                   </template>
                 </q-select>
           </q-card-section>
           <q-card-section class="q-pa-md">
-            <div class="text-left text-purple-10 text-weight-medium">
+            <div class="text-left text-primary text-weight-medium">
               Tipo de persona*
             </div>
                 <q-select filled >
                   <template v-slot:prepend>
-                    <q-avatar color="purple-10" size="sm" font-size="17px" text-color="white" icon="group" />
+                    <q-avatar color="primary" size="sm" font-size="17px" text-color="white" icon="group" />
                   </template>
                 </q-select>
           </q-card-section>
           <q-card-section class="q-pa-md" align="right">
-                 <q-btn style="background-color: #2e0742;" class="text-white" label="Limpiar" no-caps/>
-                 <q-btn class="q-ml-sm" color="purple-10" label="Crear" no-caps/>
+                 <q-btn class="text-white" color="secondary" label="Limpiar" no-caps/>
+                 <q-btn class="q-ml-sm" color="primary" label="Crear" no-caps/>
           </q-card-section>
         </q-card>
 </template>
--- src/components/CrearRemitente.vue
+++ src/components/CrearRemitente.vue
@@ -1,71 +1,71 @@
 <template>
     <q-card square flat>
           <q-card-section align="center">
-            <div class="text-h5 text-bold text-purple-10">Crear remitente</div>
+            <div class="text-h5 text-bold text-primary">Crear remitente</div>
           </q-card-section>
           <q-card-section class="q-pa-md">
-            <div class="text-left text-purple-10 text-weight-medium">
+            <div class="text-left text-primary text-weight-medium">
               Tipo de identificación*
             </div>
                 <q-select :options="tipoIdentificacion" filled >
                   <template v-slot:prepend>
-                    <q-avatar color="purple-10" size="sm" font-size="17px" text-color="white" icon="badge" />
+                    <q-avatar color="primary" size="sm" font-size="17px" text-color="white" icon="badge" />
                   </template>
                 </q-select>
           </q-card-section>
           <q-card-section class="q-pa-md">
-            <div class="text-left text-purple-10 text-weight-medium">
+            <div class="text-left text-primary text-weight-medium">
               Teléfono*
             </div>
                 <q-select filled >
                   <template v-slot:prepend>
-                    <q-avatar color="purple-10" size="sm" font-size="17px" text-color="white" icon="phone" />
+                    <q-avatar color="primary" size="sm" font-size="17px" text-color="white" icon="phone" />
                   </template>
                 </q-select>
           </q-card-section>
           <q-card-section class="q-pa-md">
-            <div class="text-left text-purple-10 text-weight-medium">
+            <div class="text-left text-primary text-weight-medium">
               Ciudad
             </div>
                 <q-select filled >
                   <template v-slot:prepend>
-                    <q-avatar color="purple-10" size="sm" font-size="17px" text-color="white" icon="location_city" />
+                    <q-avatar color="primary" size="sm" font-size="17px" text-color="white" icon="location_city" />
                   </template>
                 </q-select>
           </q-card-section>
           <q-card-section class="q-pa-md">
-            <div class="text-left text-purple-10 text-weight-medium">
+            <div class="text-left text-primary text-weight-medium">
               Barrio
             </div>
                 <q-input filled >
                   <template v-slot:prepend>
-                    <q-avatar color="purple-10" size="sm" font-size="17px" text-color="white" icon="public" />
+                    <q-avatar color="primary" size="sm" font-size="17px" text-color="white" icon="public" />
                   </template>
                 </q-input>
           </q-card-section>
           <q-card-section class="q-pa-md">
-            <div class="text-left text-purple-10 text-weight-medium">
+            <div class="text-left text-primary text-weight-medium">
               Dirección
             </div>
                 <q-input filled >
                   <template v-slot:prepend>
-                    <q-avatar color="purple-10" size="sm" font-size="17px" text-color="white" icon="map" />
+                    <q-avatar color="primary" size="sm" font-size="17px" text-color="white" icon="map" />
                   </template>
                 </q-input>
           </q-card-section>
           <q-card-section class="q-pa-md">
-            <div class="text-left text-purple-10 text-weight-medium">
+            <div class="text-left text-primary text-weight-medium">
               Correo
             </div>
                 <q-input filled >
                   <template v-slot:prepend>
-                    <q-avatar color="purple-10" size="sm" font-size="17px" text-color="white" icon="email" />
+                    <q-avatar color="primary" size="sm" font-size="17px" text-color="white" icon="email" />
                   </template>
                 </q-input>
           </q-card-section>
           <q-card-section class="q-pa-md" align="right">
-                 <q-btn style="background-color: #2e0742;" class="text-white" label="Limpiar" no-caps/>
-                 <q-btn class="q-ml-sm" color="purple-10" label="Crear" no-caps/>
+                 <q-btn class="text-white" color="secondary" label="Limpiar" no-caps/>
+                 <q-btn class="q-ml-sm" color="primary" label="Crear" no-caps/>
           </q-card-section>
         </q-card>
 </template>
